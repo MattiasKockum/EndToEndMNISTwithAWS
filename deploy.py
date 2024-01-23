06b7d187d81453b65d0a5dfc9c604362e78cc4a0
Mattias <mattias@kockum.net>
2024-01-23 15:42:22 +0100

    Demo is working.

---
+++ deploy.py
@@ -0,0 +1,77 @@
+import os
+import gzip
+import numpy as np
+
+from sagemaker.pytorch import PyTorchModel
+from sagemaker import Session
+from sagemaker.serializers import JSONSerializer
+from sagemaker.deserializers import JSONDeserializer
+
+import matplotlib.pyplot as plt
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+sess = Session()
+
+role = os.getenv("role")
+
+pt_mnist_model_data = os.getenv("pt_mnist_model_data")
+
+model = PyTorchModel(
+    entry_point="inference.py",
+    source_dir="code",
+    role=role,
+    model_data=pt_mnist_model_data,
+    framework_version="1.5.0",
+    py_version="py3",
+)
+
+predictor = model.deploy(
+    initial_instance_count=1,
+    instance_type="ml.c5.xlarge",
+    serializer=JSONSerializer(),
+    deserializer=JSONDeserializer(),
+)
+
+
+def mnist_to_numpy(data_dir="data", images_file="t10k-images-idx3-ubyte.gz", labels_file="t10k-labels-idx1-ubyte.gz"):
+
+    with gzip.open(os.path.join(data_dir, images_file), "rb") as f:
+        images = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1, 28, 28)
+
+    with gzip.open(os.path.join(data_dir, labels_file), "rb") as f:
+        labels = np.frombuffer(f.read(), np.uint8, offset=8)
+
+    return (images, labels)
+
+mnist = mnist_to_numpy()
+
+indexes = [i for i in range(len(mnist[0]))]
+np.random.shuffle(indexes)
+indexes = indexes[:16]
+
+data = {"inputs": np.expand_dims(mnist[0][indexes], axis=1).tolist()}
+
+res = predictor.predict(data)
+
+predictor.delete_endpoint()
+
+
+images = mnist[0][indexes]
+targets = mnist[1][indexes]
+
+for i in range(16):
+    image = images[i]
+    target = targets[i]
+    prediction = np.array(res[i])
+    prediction -= min(prediction)
+    prediction /= sum(prediction)
+    fig, axes = plt.subplots(1, 2)
+    axes[0].imshow(image)
+    axes[1].bar([i for i in range(10)], prediction)
+    axes[1].set_xticks([i for i in range(10)])
+    fig.show()
+    print(f"Label : {target}, Prediction : {prediction.argmax()}")
+
---
+++ launch_training.py
@@ -0,0 +1,81 @@
+import os
+from dotenv import load_dotenv
+
+import sagemaker
+from sagemaker.pytorch import PyTorch
+
+import boto3
+
+load_dotenv()
+
+sess = sagemaker.Session()
+
+role = os.getenv("role")
+
+output_path = "s3://" + sess.default_bucket() + "/DEMO-mnist"
+
+estimator = PyTorch(
+    entry_point="train.py",
+    source_dir="code",
+    role=role,
+    framework_version="1.5.0",
+    py_version="py3",
+    instance_type="ml.c5.xlarge",
+    instance_count=1,
+    volume_size=250,
+    output_path=output_path,
+    hyperparameters={
+        "batch-size": 128,
+        "epochs": 1,
+        "learning-rate": 1e-3,
+        "log-interval": 100},
+)
+
+
+def download_mnist_from_public_s3(data_dir="./data", train=True):
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    if train:
+        images_file = "train-images-idx3-ubyte.gz"
+        labels_file = "train-labels-idx1-ubyte.gz"
+    else:
+        images_file = "t10k-images-idx3-ubyte.gz"
+        labels_file = "t10k-labels-idx1-ubyte.gz"
+
+    # download objects
+    s3 = boto3.client("s3")
+    bucket = f"sagemaker-sample-files"
+    for obj in [images_file, labels_file]:
+        key = os.path.join("datasets/image/MNIST", obj)
+        dest = os.path.join(data_dir, obj)
+        if not os.path.exists(dest):
+            s3.download_file(bucket, key, dest)
+
+
+download_mnist_from_public_s3("./data", True)
+download_mnist_from_public_s3("./data", False)
+
+
+
+# Upload to the default bucket
+
+prefix = "DEMO-mnist"
+bucket = sess.default_bucket()
+loc = sess.upload_data(path="./data", bucket=bucket, key_prefix=prefix)
+
+channels = {"training": loc, "testing": loc, "validation": loc}
+
+estimator.fit(inputs=channels)
+
+pt_mnist_model_data = estimator.model_data
+print("Model artifact saved at:\n", pt_mnist_model_data)
+
+if not os.path.exists("models"):
+    os.makedirs("models")
+
+s3 = boto3.client("s3")
+l = pt_mnist_model_data.split('/')
+key = '/'.join(l[3:])
+dest = "models/" + l[-1]
+s3.download_file(bucket, key, dest)
