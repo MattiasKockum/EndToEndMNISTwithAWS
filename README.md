7bbe2404a21e819e57a17fbaa4878c9ab5f8e70c
Mattias <mattias@kockum.net>
2024-01-26 02:05:20 +0100

    better README

--- README.md
+++ README.md
@@ -14,13 +14,18 @@ Fill a .env file with your own data
 ``` python
 prefix = "DEMO-mnist"
 role = "..." # Get it from AWS
-pt_mnist_model_data = "..." You can get it by running launch_training.py
+pt_mnist_model_data = "..." You get it by running launch_training.py
 wandb_api_key = "..." Get it from Weights And Biases
 ```
 
 ``` bash
 python prepare_data.py
 python launch_training.py
+```
+
+Fill the .env file with your pt_mnist_model_data
+
+``` bash
 python deploy.py
 ```
 
--- code/train.py
+++ code/train.py
@@ -4,69 +4,23 @@ subprocess.call(["pip", "install", "wandb==0.15.11"])
 subprocess.call(["wandb", "login", os.environ["WANDB_API_KEY"]])
 
 import argparse
-import gzip
 import json
 import logging
 import sys
 import wandb
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 
 from model import Net
+from dataset import MNIST
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
-
-
-
-# Decode binary data from SM_CHANNEL_TRAINING
-# Decode and preprocess data
-# Create map dataset
-
-
-def normalize(x, axis):
-    eps = np.finfo(float).eps
-    mean = np.mean(x, axis=axis, keepdims=True)
-    # avoid division by zero
-    std = np.std(x, axis=axis, keepdims=True) + eps
-    return (x - mean) / std
-
-
-def convert_to_tensor(data_dir, images_file, labels_file):
-    """Byte string to torch tensor"""
-    with gzip.open(os.path.join(data_dir, images_file), "rb") as f:
-        images = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1, 28, 28).astype(np.float32)
-
-    with gzip.open(os.path.join(data_dir, labels_file), "rb") as f:
-        labels = np.frombuffer(f.read(), np.uint8, offset=8).astype(np.int64)
-
-    # normalize the images
-    images = normalize(images, axis=(1, 2))
-
-    # add channel dimension (depth-major)
-    images = np.expand_dims(images, axis=1)
-
-    # to torch tensor
-    images = torch.tensor(images, dtype=torch.float32)
-    labels = torch.tensor(labels, dtype=torch.int64)
-    return images, labels
-
-
-class MNIST(Dataset):
-    def __init__(self, data_dir, images_file="images.gz", labels_file="labels.gz"):
-        self.images, self.labels = convert_to_tensor(data_dir, images_file, labels_file)
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        return self.images[idx], self.labels[idx]
 
 
 def train(args):
